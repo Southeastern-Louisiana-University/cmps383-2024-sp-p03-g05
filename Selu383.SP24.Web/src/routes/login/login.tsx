6a14474b53966505f1548822e6ff7b862c142863
Joshua Cantu <joshua@thecantu.com>
2024-02-27 12:09:53 -0600

    Added login page, not linked yet though

---
+++ Selu383.SP24.Web/src/routes/login/login.tsx
@@ -0,0 +1,11 @@
+import Header from "../../elements/NavigationBar.tsx"
+import Footer from "../../elements/footer.tsx";
+
+export default function login(){
+    return (
+        <>
+        <Header/>
+        <Footer/>
+        </>
+    )
+}
