408b6c8fb8e24263476e639e63692c49b460a4c6
Joshua Cantu <joshua@thecantu.com>
2024-02-27 11:50:15 -0600

    Changed importing order

--- Selu383.SP24.Web/src/main.tsx
+++ Selu383.SP24.Web/src/main.tsx
@@ -8,14 +8,14 @@ import {
 
 
 // Importing CSS //
-import './index.css'
 import 'bootstrap/dist/css/bootstrap.css';
+import './index.css'
 
 // Importing Pages //
 import App from './App.tsx'
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
-import Header from "./routes/header.tsx"
+import Header from "./elements/NavigationBar.tsx"
 import SignUp from './routes/sign-up.tsx';
 import Home from './routes/home.tsx';
 
