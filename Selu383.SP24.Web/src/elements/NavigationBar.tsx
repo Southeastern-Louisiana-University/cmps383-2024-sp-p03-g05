a71980c240d80ac70c5a2ad7afed330790f930cb
Joshua Cantu <joshua@thecantu.com>
2024-02-27 11:50:40 -0600

    Moved to assests folder

---
+++ Selu383.SP24.Web/src/elements/NavigationBar.tsx
@@ -0,0 +1,28 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+
+
+export default function NavigationBar() {
+    return (
+        <>
+        <Navbar bg="dark" data-bs-theme="dark">
+        <Container >
+          <Navbar.Brand className='header-text' href="/">EnStay</Navbar.Brand>
+          <Nav className="me-auto " >
+            <Nav.Link className='header-text' href="/">Home</Nav.Link>
+            <Nav.Link className='header-text' href="/hotels">Browse Hotels</Nav.Link>
+            <Nav.Link className='header-text' href="/appointments">Make an Appointment</Nav.Link>
+          </Nav>
+          <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+          <Nav.Link className='header-text' href="/sign-up">SignUp / Login</Nav.Link>
+          </Navbar.Text>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+        </>
+    );
+}
