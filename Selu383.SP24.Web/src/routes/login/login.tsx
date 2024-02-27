c8cd8d991302e4ac15ec1f5c847e5094d05dc821
Joshua Cantu <joshua@thecantu.com>
2024-02-27 14:26:04 -0600

    Formatting

--- Selu383.SP24.Web/src/routes/login/login.tsx
+++ Selu383.SP24.Web/src/routes/login/login.tsx
@@ -13,28 +13,28 @@ export default function login() {
                     <h1>Let's Get you back in Paradise</h1>
                     <div>
                         <Form.Label htmlFor="inputUsername">Username / Email</Form.Label>
-                    <Form.Control
-                        type="text"
-                        id="inputUsername"
-                    />
+                        <Form.Control
+                            type="text"
+                            id="inputUsername"
+                        />
                     </div>
-                    
+
                     <br />
                     <div>
-                       <Form.Label htmlFor="inputPassword">Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        id="inputPassword"
-                    /> 
+                        <Form.Label htmlFor="inputPassword">Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            id="inputPassword"
+                        />
                     </div>
-                    
+
 
                     <br />
                     <div>
                         <Button variant="secondary background-3">Let's Get Booking</Button>{' '}
                     </div>
                     <br />
-                    
+
                 </div>
                 <div className="col-1"></div>
                 <div className="col-4">
