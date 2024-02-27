8b3f256e0c424c51b919b953b1c9984f78fe3b66
Joshua Cantu <joshua@thecantu.com>
2024-02-27 15:42:34 -0600

    Added better Error Page

--- Selu383.SP24.Web/src/error-page.tsx
+++ Selu383.SP24.Web/src/error-page.tsx
@@ -1,4 +1,5 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
+import sadFace from "./assets/sad-face-icon.svg";
 
 export default function ErrorPage() {
   const error: any = useRouteError();
@@ -7,7 +8,9 @@ export default function ErrorPage() {
   return (
     <div id="error-page">
       <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <p>This Page has not been Completed Yet</p>
+      <img src={sadFace} style={{height: 300}} />
+      <h2> <Link to={'/'}> Click Here to Go back Home</Link> </h2>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
