3af7108ba4912a59f48bf970a3aab2b019f45a8c
Joshua Cantu <joshua@thecantu.com>
2024-02-27 15:59:46 -0600

    Centered and Brought in Header and Footer

--- Selu383.SP24.Web/src/error-page.tsx
+++ Selu383.SP24.Web/src/error-page.tsx
@@ -1,5 +1,7 @@
 import { Link, useRouteError } from "react-router-dom";
 import sadFace from "./assets/sad-face-icon.svg";
+import Footer from "./elements/footer";
+import NavigationBar from "./elements/NavigationBar";
 
 export default function ErrorPage() {
   const error: any = useRouteError();
@@ -7,13 +9,16 @@ export default function ErrorPage() {
 
   return (
     <div id="error-page">
-      <h1>Oops!</h1>
-      <p>This Page has not been Completed Yet</p>
+      <NavigationBar/>
+      <center>
+        <h1>Oops!</h1>
+      <p>Something When Wrong</p>
       <img src={sadFace} style={{height: 300}} />
-      <h2> <Link to={'/'}> Click Here to Go back Home</Link> </h2>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
+      </center>
+      <Footer/>
     </div>
   );
 }
