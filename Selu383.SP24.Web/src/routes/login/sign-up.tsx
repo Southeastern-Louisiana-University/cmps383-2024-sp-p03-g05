d6827c4d58181432b9ecd48e558ed0d268851f08
CaptainSpatula <brycenorris2002@gmail.com>
2024-02-28 11:17:58 -0600

    Fixed up the sign-up page CSS

--- Selu383.SP24.Web/src/routes/login/sign-up.tsx
+++ Selu383.SP24.Web/src/routes/login/sign-up.tsx
@@ -7,36 +7,56 @@ const SignUp = () => {
     <>
       <Header />
       <div className="row background-2">
-        <h1 className="center">Account Creation</h1>
-        <div className="col-md-3 background-1"></div>
+        <div className="col-md-1 background-1"></div>
+        <div className="col-md-2 background-2"></div>
         <Col md={6}>
+          <h1 className="center">Account Creation</h1>
           <div className="text-center">
             <Form.Label htmlFor="inputUsername">Email</Form.Label>
-            <Form.Control type="text" id="inputUsername" className="text-center border border-dark" />
+            <Form.Control
+              type="text"
+              id="inputUsername"
+              className="text-center border border-dark"
+            />
           </div>
           <br />
           <div className="text-center">
             <Form.Label htmlFor="inputPassword">Password</Form.Label>
-            <Form.Control type="password" id="inputPassword" className="text-center border border-dark" />
+            <Form.Control
+              type="password"
+              id="inputPassword"
+              className="text-center border border-dark"
+            />
           </div>
           <br />
           <div className="text-center">
             <Form.Label htmlFor="inputFirstname">First Name</Form.Label>
-            <Form.Control type="text" id="inputFirstname" className="text-center border border-dark" />
+            <Form.Control
+              type="text"
+              id="inputFirstname"
+              className="text-center border border-dark"
+            />
           </div>
           <br />
           <div className="text-center">
             <Form.Label htmlFor="inputLastname">Last Name</Form.Label>
-            <Form.Control type="text" id="inputLastname" className="text-center border border-dark" />
+            <Form.Control
+              type="text"
+              id="inputLastname"
+              className="text-center border border-dark"
+            />
           </div>
           <br />
           <div className="text-center">
             <Button variant="secondary background-1">Create Account</Button>
+            <div className="pb-2"></div>
           </div>
         </Col>
-        <div className="col-md-3 background-1"></div>
+        <div className="col-md-2 background-2"></div>
+        <div className="col-md-1 background-1"></div>
+        
       </div>
-      <div className="pb-4"></div>
+
       <Footer />
     </>
   );
