55c7a8e6b51d9990c309c5ac0c9a9f4b3b0254d0
Luke Norton <luke.norton@selu.edu>
2024-03-07 11:53:19 -0600

    added endpoint to search for hotels by typing in a name or address. Does not have to have entire name input

--- Selu383.SP24.Api/Data/DataContext.cs
+++ Selu383.SP24.Api/Data/DataContext.cs
@@ -6,6 +6,9 @@ using Selu383.SP24.Api.Features.HotelReservations;
 using Selu383.SP24.Api.Features.HotelRoom;
 using Selu383.SP24.Api.Features.Hotels;
 using Selu383.SP24.Api.Features.HotelServices;
+using NetTopologySuite.Geometries;
+
+
 
 namespace Selu383.SP24.Api.Data;
 
@@ -18,18 +21,21 @@ public class DataContext : IdentityDbContext<User, Role, int, IdentityUserClaim<
     public DataContext()
     {
     }
-
+    public virtual DbSet<ServiceRequest> ServiceRequests { get; set; }
+    public virtual DbSet<UniversalStatus> UniversalStatuses { get; set; }
+    public virtual DbSet<Room> Rooms { get; set; }
+    public virtual DbSet<RoomPackage> RoomsPackage { get; set; }
+    public virtual DbSet<Reservation> Reservations { get; set; }
+    public virtual DbSet<Hotel> Hotels { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+
+       
     }
-    public virtual DbSet<ServiceRequest> ServiceRequests { get; set; }
-    public virtual DbSet<UniversalStatus> UniversalStatuses { get; set; }
-    public virtual DbSet<Room> Rooms { get; set; }
-    public virtual DbSet<RoomPackage> RoomsPackage { get; set;}
-    public virtual DbSet<Reservation> Reservations { get; set; }
+
 
 }
 
