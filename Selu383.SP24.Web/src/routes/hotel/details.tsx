16bfbe8069de9ca1cb48112b9ee3eb0c0ed99d8e
Joshua Cantu <joshua@thecantu.com>
2024-03-13 22:54:01 -0500

    Added feature abily to get packges

--- Selu383.SP24.Web/src/routes/hotel/details.tsx
+++ Selu383.SP24.Web/src/routes/hotel/details.tsx
@@ -1,9 +1,13 @@
-import { useParams } from "react-router-dom";
+import { Outlet, useParams } from "react-router-dom";
 import Footer from "../../elements/footer";
 import { useEffect, useState } from "react";
 import mountain from "../../assets/mountain-hole.jpg"
 
 import { HotelDto } from "../../features/hotels/HotelDto";
+import { Button, Form } from "react-bootstrap";
+import { roomGetDto } from "../../features/rooms/roomGetDto";
+import { packageGetDto } from "../../features/package/packagesGetDto";
+
 
 
 
@@ -14,6 +18,8 @@ export default function hotelDetails() {
     const { id } = useParams();
 
     const [hotel, setHotel] = useState<HotelDto>();
+
+    const [packages, setpackages] = useState<packageGetDto[]>([]);
 
 
     useEffect(() => {
@@ -26,37 +32,60 @@ export default function hotelDetails() {
             });
     });
 
+    useEffect(() => {
+        fetch("/api/packages/", {
+            method: "get",
+        })
+            .then<packageGetDto[]>((r) => r.json())
+            .then((j) => {
+                setpackages(j);
+            });
+    });
+
 
     return (
         <>
             <div style={{ backgroundImage: `url(${mountain})` }}>
-<div className="container">
-                <div className="row" >
-                    <div className="col-1"></div>
-
-                    <div className="col-10" >
-                        
-                            <>
-                                <br />
+                <div className="container">
+                    <div className="row" >
+                        <div className="col-1"></div>
+                        <div className="col-10" >
+                            <>                                
+                            <br />
                                 <div className="container">
                                     <div className="row" style={{ backgroundColor: 'rgba(255,255,255,.80)' }}>
                                         <div className="col-1"></div>
-
-
-
                                         <div className="col-10">
                                             <div >
                                                 <br />
                                                 <center><h1> Welcome to {hotel?.name}</h1>
-                                                <h3>located at {hotel?.address}</h3>
-                                                <p>Please Enjoy your Stay</p></center>
+                                                    <h3>located at {hotel?.address}</h3>
+                                                    <p>Please Enjoy your Stay</p>
+                                                <h3>When should we be Expeting you? </h3></center>
                                                 
+                                                <br />
+                                                <div className="row">
+                                                    <div className="col-1"></div>
+                                                    <div className="col-4">
+                                                        <h4>Check In</h4>
+                                                        <Form.Control type="date" />
+                                                    </div>
+                                                    <div className="col-2">
+                                                    </div>
+                                                    <div className="col-4">
+                                                        <h4>Check Out</h4>
+                                                        <Form.Control type="date" />
+                                                    </div>
+                                                    <div>
+                                                        <br />
+                                                        <center>
+                                                            <Button variant="secondary background-1">Let's See What's Available</Button>{" "}
+                                                        </center>
+                                                        <br />
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
-                                        
-
-
-
                                         <div className="col-1"></div>
                                     </div>
                                 </div>
@@ -64,19 +93,24 @@ export default function hotelDetails() {
 
                                 <br />
                             </>
-                      
+
+                        </div>
+
+                        <div className="col-1"></div>
                     </div>
 
-                    <div className="col-1"></div>
+
                 </div>
-
-
             </div>
-            </div>
-            
 
+            {packages?.map((roomPackage) => (
+                <div>
+                    {roomPackage.Description}
+                </div>
+            ))}
 
             <Footer />
         </>
     );
 }
+
