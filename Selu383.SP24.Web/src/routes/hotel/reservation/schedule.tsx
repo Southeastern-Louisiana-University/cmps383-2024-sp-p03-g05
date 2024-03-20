621e762659d68812c7bc6cfc199fb4650359320b
Joshua Cantu <joshua@thecantu.com>
2024-03-19 21:38:05 -0500

    Added conditional button

--- Selu383.SP24.Web/src/routes/hotel/reservation/schedule.tsx
+++ Selu383.SP24.Web/src/routes/hotel/reservation/schedule.tsx
@@ -2,20 +2,20 @@ import { useState, useEffect } from "react";
 import { packageGetDto } from "../../../features/package/packagesGetDto";
 import { Link, useParams, useSearchParams } from "react-router-dom";
 import { Button } from "react-bootstrap";
-import hotel from "..";
 
 
 export default function schedule() {
+
     const [packages, setpackages] = useState<packageGetDto[]>([]);
     const [params] = useSearchParams();
     const { id } = useParams();
-    const checkInDateArray = params.get("checkInDate")?.split("-")?? [];
-    const checkInDate:Date = new Date(parseInt(checkInDateArray[0]), parseInt(checkInDateArray[1]) -1, parseInt(checkInDateArray[2])); 
-    
-    const checkOutDateArry = params.get("checkOutDate")?.split("-")?? [];
-    const checkOutDate:Date = new Date(parseInt(checkOutDateArry[0]), parseInt(checkOutDateArry[1]) -1, parseInt(checkOutDateArry[2])); 
+    const checkInDateArray = params.get("checkInDate")?.split("-") ?? [];
+    const checkInDate: Date = new Date(parseInt(checkInDateArray[0]), parseInt(checkInDateArray[1]) - 1, parseInt(checkInDateArray[2]));
 
-    
+    const checkOutDateArry = params.get("checkOutDate")?.split("-") ?? [];
+    const checkOutDate: Date = new Date(parseInt(checkOutDateArry[0]), parseInt(checkOutDateArry[1]) - 1, parseInt(checkOutDateArry[2]));
+
+
 
     useEffect(() => {
         fetch(`/api/room/GetAllAvailablePackages?hotelId=${id}`, {
@@ -29,41 +29,39 @@ export default function schedule() {
 
     return (
         <>
-        <center>
+            <center>
 
-            <h4>{checkInDate.toDateString()} - {checkOutDate.toDateString()}</h4>
-        </center>
+                <h4>{checkInDate.toDateString()} - {checkOutDate.toDateString()}</h4>
+            </center>
 
             {packages?.map((roomPackage) => (
                 <>
-                <div>
-                    <div className="container">
-                        <div className="row " style={{ backgroundColor: 'rgba(201, 218, 234, .60)' }}>
-                            <div className="col-1"></div>
-                            <div className="col-4 ">
-                                <div >
-                                    <br />
-                                    <h2>{roomPackage.title}</h2>
-                                    <p>{roomPackage.description}</p>
+                    <div>
+                        <div className="container">
+                            <div className="row " style={{ backgroundColor: 'rgba(201, 218, 234, .60)' }}>
+                                <div className="col-1"></div>
+                                <div className="col-4 ">
+                                    <div >
+                                        <br />
+                                        <h2>{roomPackage.title}</h2>
+                                        <p>{roomPackage.description}</p>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="col-4">
-                                <br />
-                                <p>Starting at : ${roomPackage.startingPrice}/Night</p>
-                            </div>
-                            <div className="col-2 ">
-                  
-                                    <Link to={`/hotels/details/${roomPackage.id}`}>
+                                <div className="col-4">
+                                    <br />
+                                    <p>Starting at : ${roomPackage.startingPrice}/Night</p>
+                                </div>
+                                <div className="col-2 ">
+                                    <BookButton id={roomPackage.id} />
+                                    {/* <Link to={`/hotels/details/${roomPackage.id}`}>
                                     <Button variant="outline-success ">Book my Suite</Button>{" "}
-                                </Link>
-                       
-                                
+                                </Link> */}
+                                </div>
+                                <div className="col-1"></div>
                             </div>
-                            <div className="col-1"></div>
                         </div>
+                        <br />
                     </div>
-                   <br />
-                </div>
 
                 </>
 
@@ -71,4 +69,39 @@ export default function schedule() {
 
         </>
     )
+}
+
+
+function BookButton({id}:{id: number}) {
+    const [isAuthed, setIsAuthed] = useState(false);
+
+    useEffect(() => {
+        fetch(`/api/authentication/me`, {
+            method: "get",
+        })
+            .then(response => { if (response.ok) { setIsAuthed(true) } })
+    }, []);
+
+
+    if (isAuthed) {
+        return (
+            <>
+                <Link to={`/hotels/details/${id}`}>
+                    <Button variant="outline-success ">Book my Suite</Button>{" "}
+
+                </Link>
+            </>
+        )
+    }
+    else {
+        return (
+            <>
+            <Link to={`/login`}>
+                <Button variant="outline-success ">Make login Model</Button>{" "}
+            </Link>
+            </>
+        )
+
+    }
+
 }
